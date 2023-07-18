5f509ea8b9c19cf825a7f891d9d1722307b0e355
Andrés Triana <wtriana@outlook.com>
2023-07-17 23:13:56 -0500

    Estilización de la section BlogSection con Bootstrap, se agregan imágenes a cada PostCard y se estilizan los botones

--- frontend/src/components/sections/BlogSection.js
+++ frontend/src/components/sections/BlogSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function PostCard({ title, category }) {
+function PostCard({ title, category, image }) {
 	return (
 		<div className="col-md-5">
 			<div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
@@ -15,28 +15,15 @@ function PostCard({ title, category }) {
 						This is a wider card with supporting text below as a
 						natural lead-in to additional content.
 					</p>
-					<Link to="#" className="icon-link gap-1 icon-link-hover stretched-link">
-						Continue reading
-						<i class="fa-solid fa-chevron-right"></i>
-					</Link>
+					<div>
+						<Link to="#" className="btn btn-primary fs-5" role="button">
+							Leer más
+							<i className="fa-solid fa-chevron-right"></i>
+						</Link>
+					</div>
 				</div>
 				<div className="col-auto d-none d-lg-block">
-					<svg
-						className="bd-placeholder-img"
-						width="200"
-						height="250"
-						xmlns="http://www.w3.org/2000/svg"
-						role="img"
-						aria-label="Placeholder: Thumbnail"
-						preserveAspectRatio="xMidYMid slice"
-						focusable="false"
-					>
-						<title>Placeholder</title>
-						<rect width="100%" height="100%" fill="#55595c"></rect>
-						<text x="50%" y="50%" fill="#eceeef" dy=".3em">
-							Thumbnail
-						</text>
-					</svg>
+					<img className="bd-placeholder-img" width="200" height="250" alt="imagen" src={image}/>
 				</div>
 			</div>
 		</div>
@@ -46,10 +33,14 @@ function PostCard({ title, category }) {
 function BlogSection() {
 	return (
 		<section className="row mb-2 justify-content-center">
-			<PostCard title="Featured post" category="World" />
-			<PostCard title="Post title" category="Design" />
-			<PostCard title="Gatitos" category="Salud" />
-			<PostCard title="Perritos" category="Vacunas" />
+			<h2 className="mb-5">Publicaciones recientes</h2>
+			<PostCard title="Featured post" category="World" image="images/photos/veterinarian-4.jpg" />
+			<PostCard title="Post title" category="Design" image="images/photos/veterinarian-13.jpg" />
+			<PostCard title="Gatitos" category="Salud" image="images/photos/veterinarian-7.jpg" />
+			<PostCard title="Perritos" category="Vacunas" image="images/photos/veterinarian-11.jpg"/>
+			<div>
+				<button className="btn btn-primary fs-5 mt-5" type="button">Ver todas las publicaciones</button>
+			</div>
 		</section>
 	);
 }
